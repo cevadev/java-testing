8b5e2aac91acac568be3da6b3ce6e8b3967b0c71
cevadev <carlos.villanueva.altuna@gmail.com>
2023-11-09 19:11:39 -0300

    Java Testing - Module 1

--- src/main/java/com/edteam/testing/Calculadora.java
+++ src/main/java/com/edteam/testing/Calculadora.java
@@ -1,10 +1,23 @@
 package com.edteam.testing;
 
 public class Calculadora {
+    public double calcularIMC(double peso, double altura)throws InterruptedException{
+        if(peso == 0 || altura == 0)
+            throw new IllegalArgumentException("No se aceptan valores con cero");
+        double imc = peso / (altura * altura);
+        imc = Math.round(imc * 1000);
+        imc = imc / 1000;
+
+        // agregamos esta linea para hacer la prueba de Test Timeout
+        Thread.sleep(1000L);
+        return imc;
+    }
+/*
     public double calcularIMC(double peso, double altura){
         double imc = peso / (altura * altura);
         imc = Math.round(imc * 1000);
         imc = imc / 1000;
         return imc;
     }
+*/
 }
